54fc1890fe3cd82c3356539b29848daaa5296dcc
MaxQNEI <maxqnei@gmail.com>
2018-05-04 15:15:38 +0300

    Convert tabulation to spaces GUID.js

--- GUID.js
+++ GUID.js
@@ -1,10 +1,10 @@
 'use strict';
 
 Object.defineProperties(window, {
-	GUID: { enumerable: true, value: function GUID() {
-		function s4() {
-			return Math.round((Math.random()+1)*1e5).toString(16).substr(1,4);
-		}
-		return ([s4(),s4(),'-',s4(),'-',s4(),'-',s4(),'-',s4(),s4(),s4()]).join('');
-	} },
+    GUID: { enumerable: true, value: function GUID() {
+        function s4() {
+            return Math.round((Math.random()+1)*1e5).toString(16).substr(1,4);
+        }
+        return ([s4(),s4(),'-',s4(),'-',s4(),'-',s4(),'-',s4(),s4(),s4()]).join('');
+    } },
 });
