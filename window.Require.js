1ffa538e54aadbad8880d922a96444747550b0c4
MaxQNEI <maxqnei@gmail.com>
2018-05-04 15:16:15 +0300

    Convert tabulation to spaces window.Require.js

--- window.Require.js
+++ window.Require.js
@@ -1,16 +1,16 @@
 'use strict';
 
 Object.defineProperties(window, {
-	Require: { enumerable: true, value: function Require(name, callback) {
-		console.log(`window.Require()`);
+    Require: { enumerable: true, value: function Require(name, callback) {
+        console.log(`window.Require()`);
 
-		var script = document.createElement('script');
-		script.type = 'text/javascript';
-		script.src = name;
-		script.addEventListener('load', callback || function callback() {
-			console.log(`window.Require(): ${this.src} loaded.`);
-		});
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = name;
+        script.addEventListener('load', callback || function callback() {
+            console.log(`window.Require(): ${this.src} loaded.`);
+        });
 
-		document.head.appendChild(script);
-	} }
+        document.head.appendChild(script);
+    } }
 });
